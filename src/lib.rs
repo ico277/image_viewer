5aed081c47a04e9b1a2fd95391119cf562364ba7
ico277 <58118556+ico277@users.noreply.github.com>
2021-07-27 13:49:18 +0200

    version 2.1.1
    - cleaned the code a bit

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,123 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+use image::ImageBuffer;
+use std::iter::Iterator;
+
+pub const VERSION: &str = "2.1.1";
+
+pub struct Settings {
+    pub image_file: String,
+    pub downscale_ratio_per_pix: f32,
+    pub gif_loop: bool,
+}
+
+pub fn parse_file(settings: &mut Settings, iter: &mut dyn Iterator<Item = &str>) {
+    for i in iter {
+        match i.split_once("=") {
+            Some(("img", s)) => {
+                settings.image_file = s.to_string();
+            }
+            Some(("downscale_ratio", s)) => match String::from(s).parse() {
+                Ok(r) => settings.downscale_ratio_per_pix = r,
+                Err(err) => {
+                    eprintln!(
+                        "Warning: Unable to parse downscale_ratio in the settings: {}",
+                        err
+                    );
+                    eprintln!("Warning: Using default amount of {}", 1.5_f32);
+                    settings.downscale_ratio_per_pix = 1.5_f32;
+                }
+            },
+            Some(("gif_loop", s)) => match s {
+                "true" => settings.gif_loop = true,
+                "false" => settings.gif_loop = false,
+                _ => (),
+            },
+            None => break,
+            _ => continue,
+        }
+    }
+}
+
+pub fn parse_args(settings: &mut Settings, iter: &mut dyn Iterator<Item = String>) {
+    //Iterates the iterator once to remove the first argument
+    iter.next();
+
+    //Match the first argument as the file
+    match iter.next().unwrap_or(String::from(" ")).as_str() {
+        "-v" | "--version" => {
+            println!("image_viewer v{}", VERSION);
+            std::process::exit(0);
+        },
+        " " => (),
+        s => {
+            settings.image_file = String::from(s);
+        }
+    }
+
+    for i in iter {
+        match i.split_once("=") {
+            Some(("img", s)) => {
+                settings.image_file = s.to_string();
+            }
+            Some(("downscale_ratio", s)) => match s.parse() {
+                Ok(r) => settings.downscale_ratio_per_pix = r,
+                Err(err) => {
+                    eprintln!("Warning: Unable to parse downscale_ratio: {}", err);
+                    eprintln!("Warning: Using default amount of {}", 2_f32);
+                    settings.downscale_ratio_per_pix = 2_f32;
+                }
+            },
+            Some(("gif_loop", s)) => match s {
+                "true" => settings.gif_loop = true,
+                "false" => settings.gif_loop = false,
+                _ => (),
+            },
+            None => break,
+            _ => continue,
+        }
+    }
+}
+
+/*fn resize_pluto(width: i32, height: i32) -> pluto_lib_t {
+    unsafe {
+        _pluto_canvas.cheight = height;
+        _pluto_canvas.height = height / 4;
+
+        _pluto_canvas.cwidth = width;
+        _pluto_canvas.width = width / 2;
+
+        //pluto_resize();
+
+        _pluto_canvas
+    }
+}*/
+
+pub fn display_image(img: ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>) {
+    unsafe {
+        //resize_pluto(img.width() as i32, img.height() as i32);
+        //println!("cheight:{} cwidth:{}", _pluto_canvas.cheight, _pluto_canvas.cwidth);
+        //println!("height:{} width:{}", img.height(), img.width());
+        for x in 0..img.width() as u32 {
+            //Loops through every pixel in the row
+            for y in 0..img.height()  as u32 {
+                //Gets color data and prints it
+                let color = img.get_pixel(x, y).0;
+                pluto_set_cpix(x as i32, y as i32, color[0], color[1], color[2]);
+            }
+        }
+        pluto_write_out();
+        pluto_render();
+    }
+}
+
+pub fn clean_exit() {
+    // Deinits pluto
+    unsafe {
+        pluto_deinit();
+    }
+    //Resets the color
+    print!("\x1b[0m");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,129 +3,29 @@ use image_viewer::*;
 use image::gif::GifDecoder;
 use image::imageops::{resize, FilterType};
 use image::io::Reader as ImageReader;
-use image::{AnimationDecoder, ImageBuffer};
+use image::AnimationDecoder;
 
 use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Read;
-use std::iter::Iterator;
 use std::path::PathBuf;
 use std::{thread, time};
 
 use dirs::home_dir;
 
-struct Settings {
-    image_file: String,
-    downscale_ratio_per_pix: f32,
-    gif_loop: bool,
-}
-
-fn parse_file(settings: &mut Settings, iter: &mut dyn Iterator<Item = &str>) {
-    for i in iter {
-        match i.split_once("=") {
-            Some(("img", s)) => {
-                settings.image_file = s.to_string();
-            }
-            Some(("downscale_ratio", s)) => match String::from(s).parse() {
-                Ok(r) => settings.downscale_ratio_per_pix = r,
-                Err(err) => {
-                    eprintln!(
-                        "Warning: Unable to parse downscale_ratio in the settings: {}",
-                        err
-                    );
-                    eprintln!("Warning: Using default amount of {}", 1.5_f32);
-                    settings.downscale_ratio_per_pix = 1.5_f32;
-                }
-            },
-            Some(("gif_loop", s)) => match s {
-                "true" => settings.gif_loop = true,
-                "false" => settings.gif_loop = false,
-                _ => (),
-            },
-            None => break,
-            _ => continue,
-        }
-    }
-}
-
-fn parse_args(settings: &mut Settings, iter: &mut dyn Iterator<Item = String>) {
-    //Iterates the iterator once to remove the first argument
-    iter.next();
-
-    //Match the first argument as the file
-    match iter.next() {
-        Some(s) => {
-            settings.image_file = s;
-        }
-        None => (),
-    }
-
-    for i in iter {
-        match i.split_once("=") {
-            Some(("img", s)) => {
-                settings.image_file = s.to_string();
-            }
-            Some(("downscale_ratio", s)) => match s.parse() {
-                Ok(r) => settings.downscale_ratio_per_pix = r,
-                Err(err) => {
-                    eprintln!("Warning: Unable to parse downscale_ratio: {}", err);
-                    eprintln!("Warning: Using default amount of {}", 2_f32);
-                    settings.downscale_ratio_per_pix = 2_f32;
-                }
-            },
-            Some(("gif_loop", s)) => match s {
-                "true" => settings.gif_loop = true,
-                "false" => settings.gif_loop = false,
-                _ => (),
-            },
-            None => break,
-            _ => continue,
-        }
-    }
-}
-
-fn display_image(img: ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>) {
-    unsafe {
-        for y in 0..img.height() {
-            //Loops through every pixel in the row
-            for x in 0..img.width() {
-                //Gets color data and prints it
-                let color = img.get_pixel(x, y).0;
-                //print!("\x1b[38;2;{};{};{}m{}", color[0], color[1], color[2], settings.symbol);
-                pluto_set_cpix(x as i32, y as i32, color[0], color[1], color[2]);
-            }
-        }
-        pluto_write_out();
-        pluto_render();
-    }
-}
-
-fn clean_exit() {
-    // Deinits pluto
-    unsafe {
-        pluto_deinit();
-    }
-    //Resets the color
-    print!("\x1b[0m");
-}
-
 fn main() {
     ctrlc::set_handler(move || clean_exit()).expect("Error setting Ctrl-C handler");
     std::io::stdout();
-    unsafe {
-        pluto_init_window();
-    }
+
     let mut settings = Settings {
         image_file: String::new(),
         downscale_ratio_per_pix: 2_f32,
         gif_loop: false,
     };
+
     let term_size;
-    unsafe {
-        term_size = [_pluto_canvas.cwidth as f32, _pluto_canvas.cheight as f32];
-    }
 
     //Parsing config file
     let mut user_dir = match home_dir() {
@@ -218,7 +118,6 @@ fn main() {
     //Parsing args
     parse_args(&mut settings, &mut env::args());
     //println!("Settings: {{\n\timage_file: '{}'\n\tsymbol: '{}'\n\tdownscale_ratio: '{}'\n}}", settings.image_file, settings.symbol, settings.downscale_ratio_per_pix);
-
     //Checking if image file is an animated GIF
     if settings.image_file.to_lowercase().ends_with(".gif") {
         // Decode a gif into frames
@@ -239,6 +138,10 @@ fn main() {
                 std::process::exit(1);
             }
         };
+        unsafe {
+            pluto_init_window();
+            term_size = [_pluto_canvas.cwidth as f32, _pluto_canvas.cheight as f32];
+        }
         let frames = decoder.into_frames();
         let frames = match frames.collect_frames() {
             Ok(m) => m,
@@ -251,10 +154,10 @@ fn main() {
             }
         };
         if settings.gif_loop {
-            loop {
+            /*loop {
                 eprintln!("Error: gif_loop not implemented yet!");
                 std::process::exit(1);
-            } 
+            }*/
         } else {
             for frame in frames {
                 let time = time::Duration::from_millis(frame.delay().numer_denom_ms().0 as u64);
@@ -263,9 +166,9 @@ fn main() {
                 if img_dimensions[0] <= term_size[0] && img_dimensions[1] <= term_size[1] {
                     let downscale_ratio = term_size[0] / img_dimensions[0];
                     let new_width =
-                        (img_dimensions[0] * downscale_ratio) / settings.downscale_ratio_per_pix;
+                        img_dimensions[0] * downscale_ratio;
                     let new_height =
-                        (img_dimensions[1] * downscale_ratio) / settings.downscale_ratio_per_pix;
+                        img_dimensions[1] * downscale_ratio;
                     img = resize(
                         &img,
                         new_width.floor() as u32,
@@ -295,26 +198,31 @@ fn main() {
                 std::process::exit(1);
             }
         };
-        //Clears console and moves the curser to the start
-        print!("\x1b[2J\x1b[H");
+        unsafe {
+            pluto_init_window();
+            term_size = [_pluto_canvas.cwidth as f32, _pluto_canvas.cheight as f32];
+        }
 
         //Gets image data and dimensions of the image
-        let img = img.into_rgba8();
+        let mut img = img.into_rgba8();
         let img_dimensions = [img.width() as f32, img.height() as f32];
 
         //NOTE downScaleRatio = (termWidth/fullImgWidth); resize(img, fullImgWidth*downScaleRatio, fullImgHeight*downScaleRatio);
         //Calculates the downscale ratio
-        let downscale_ratio = term_size[0] / img_dimensions[0];
-        let new_width = (img_dimensions[0] * downscale_ratio) / settings.downscale_ratio_per_pix;
-        let new_height = (img_dimensions[1] * downscale_ratio) / settings.downscale_ratio_per_pix;
-
-        //Downscales the image and displays the new size of the image
-        let img = resize(
-            &img,
-            new_width.floor() as u32,
-            new_height.floor() as u32,
-            FilterType::Nearest,
-        );
+        //println!("{}p x {}p", term_size[0], term_size[1]);
+        if  term_size[0] < img_dimensions[0] || term_size[1] < img_dimensions[1] {
+            let downscale_ratio = term_size[0] / img_dimensions[0];
+            let new_width =
+                img_dimensions[0] * downscale_ratio;
+            let new_height =
+                img_dimensions[1] * downscale_ratio;
+            img = resize(
+                &img,
+                new_width.floor() as u32,
+                new_height.floor() as u32,
+                FilterType::Nearest,
+            );
+        }
         let img_dimensions_downscaled = [img.width(), img.height()];
         println!(
             "{}p x {}p (downscaled to {}p x {}p)",
